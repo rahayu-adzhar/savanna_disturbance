855657b8b8b448057258d511329b94883e4c1280
Rahayu Adzhar <rahayu@umd.edu>
2024-07-10 13:21:21 -0400

    update to function to improve degradation event performance but need to sort out drop v rise timing

--- regression_line_modelling.R
+++ regression_line_modelling.R
@@ -14,8 +14,6 @@ fw24<- read.csv("tcc_tch_15_23.csv")
 fw24$cat <- as.character(fw24$strata)
 
 
-
-
 h_check <- fw24[c('TCH15', 'TCH16', 'TCH17', 'TCH18', 'TCH19', 'TCH20', 'TCH21', 'TCH22', 'TCH23')]
 
 c_check <- fw24[c('TCC15', 'TCC16', 'TCC17', 'TCC18', 'TCC19', 'TCC20', 'TCC21', 'TCC22', 'TCC23')]
@@ -42,17 +40,17 @@ c_check$id <- c(1:89)
 
 hc_check <- merge(h_check, c_check, by = "id")
 
-unchanged <- hc_check %>% filter(match_h == 0 | match_c == 0)
+changed <- hc_check %>% filter(match_h == 0 | match_c == 0)
 
 
-#we're gonna plot the path of a girlie (phase diagrams)
+#make a list of all the 
 
-name <-  c(unchanged$id)
+name <-  c(changed$id)
 row <- 1
-girlies <- list()
+site_list <- list()
 
-for(i in 1:nrow(unchanged)){{
-  plot <- unchanged[row,]
+for(i in 1:nrow(changed)){{
+  plot <- changed[row,]
   df <- c()
   df$id <- name[[i]]
   df$tcc <- as.integer(plot[12:20])
@@ -60,118 +58,93 @@ for(i in 1:nrow(unchanged)){{
   df$year <- c(15:23)
   df <- as.data.frame(df)
   
-  girlies[[i]] <- df}
+  site_list[[i]] <- df}
   row <- row + 1
   }
 
+#model accounts for every step to describe a disturbance/recovery curve
+#w assumption that all iterations of savanna behaviour will fall somewhere on it
+
+#4 potential stages (and they aren't necessarily in this order i.e. don't always start w first listed stage)
+# pre-event : degradation has not occurred
+# event: sharp decline facilitated by degradation
+# event_floor: what it's like at the bottom
+# recovery: recovering from the event
 
 
+test_site <- site_list[[3]]
+##test_site is selected bc she does possess all 4 stages
 
-### aaaaa baby u don't gotta recreate the graph ur recreating the concept of the shape
-#what we want to tell the line
-#ur y value will be steady until u hit x value (t0)
-#where t0 is degradation start point
-
-#eq 2 is the drop between t0 and t1 (t1 being the start of the recovery curve)
-
-##eq 3 is then the recovery curve
-
-##so if t < t0, use eq1
-##else if t>t0 but t<t1, use eq2
-##else use eq3
-
-##assumption that degradation drop happens in the middle
-##assumption that recovery goes back up to initial h levels
-# test_girl <- girlies[[3]]
-# low <- min(test_girl$tcc)
-# t_rise <-(test_girl[test_girl$tcc == low,])$year
-# t_drop <- t_rise - 1
-# c_start <- test_girl$tcc[[1]]
-# c_end <- test_girl$tcc[[9]]
-# c_drop <- (test_girl[test_girl$year == t_drop,])$tcc
-# c_rise <- (test_girl[test_girl$year == t_rise,])$tcc
-# tau <- 2 #testing value when we r running u like crazy this can b adjusted i think
+#parameters to be optimised
+tau <- 2
+t_rise <- (test_site[test_site$tcc == min(test_site$tcc),])$year #opt
+t_drop <- t_rise - 1 #opt 
+c_start <- test_site$tcc[[1]] #needs to be optimised bc doesn't account for fluctuations etc et  al for the rest of em prior tot he drop
+c_end <- test_site$tcc[[9]] #opt
+c_drop <- (test_site[test_site$year == t_drop,])$tcc #opt
+c_rise <- (test_site[test_site$year == t_rise,])$tcc #opt
 
 
-
-#t_drop (year when the decline happened, assumed to be 1 year before the lowest point)
-
-
-## ok the model is meant to draw a girlie given a set of parameters (NOT FULL ON VALUES)
-##
-
-# for (i in 1:nrow(test_girl)){
-#   year <- test_girl$year[[i]]
-# if (year <= t_drop) { 
-#   out <- c_start
-# } else if (year > t_drop && year < t_rise ){
-#   a = c(c_rise : c_drop)
-#   out = sample(a,1)
-# } else{
-#   out = (c_end - c_rise)*(1 - (exp(-tau*(year - t_rise)))) + c_rise
-# } #i should be t minue t0, represent going from c-drop to c-end 
-#   #browser() #runs u thru loop by loop if u keep clicking c so u cn check girlies
-#   test_girl$out[[i]] <- out
-# }
-
-##she works beautifully now but loops are Not efficient
-##so try to make her a function
-# > yay=1:10
-# > yay <5
-# [1]  TRUE  TRUE  TRUE  TRUE FALSE FALSE FALSE FALSE FALSE FALSE
-# > yay[yay <5]
-# [1] 1 2 3 4
-# > yay[yay <5] =8
-# > yay
-# [1]  8  8  8  8  5  6  7  8  9 10
-
-#if cukup syarat do this, if not don't
-test_girl <- girlies[[3]]
-# low <- min(test_girl$tcc)
-# t_rise <-(test_girl[test_girl$tcc == low,])$year
-# t_drop <- t_rise - 1
-# c_start <- test_girl$tcc[[1]]
-# c_end <- test_girl$tcc[[9]]
-# c_drop <- (test_girl[test_girl$year == t_drop,])$tcc
-# c_rise <- (test_girl[test_girl$year == t_rise,])$tc
-
-
-
-all_curve <- function(time, site){
+all_curve <- function(time, site, tau, t_rise, t_drop, c_start, c_end, c_drop, c_rise){
   #u want to optimise girlies u dont know v well
 #to optimise is to find the most likely value
- # low <- min(site$tcc)
-  t_rise <- (site[site$tcc == low,])$year #opt
-  t_drop <- t_rise - 1 #opt
-  c_start <- site$tcc[[1]] #needs to be optimised bc doesn't account for fluctuations etc et  al for the rest of em prior tot he drop
-  c_end <- site$tcc[[9]] #opt
-  c_drop <- (site[site$year == t_drop,])$tcc #opt
-  c_rise <- (site[site$year == t_rise,])$tcc #opt
+
   #if ur dependent on other guys dont get optimised outside
   drop_m <- ((c_rise - c_drop)/(t_rise - t_drop))
   drop_w <- c_drop - drop_m*t_drop #c of y = mx + c
-  tau <- 2 #needs optimising
+
 
   out <- rep(c_start, length(time))
   out[time<= t_drop] = c_start
-  out[time>t_drop && year<t_rise] =  drop_m * time + drop_w
+  out[time>t_drop && time<t_rise] =  drop_m * time + drop_w
   out[time == t_rise] = c_rise
   out[time>t_rise] = (c_end - c_rise)*(1 - (exp(-tau*(time[time>t_rise] - t_rise)))) + c_rise
 return(out) #makes her global
   }
 
-print(all_curve(time = 15:23, site = test_girl))
+#print(all_curve(time = 15:23, site = test_girl))
+france <- test_site$tcc
+
+new_fit <- nls( france ~ all_curve(time = 15:23, site = test_site, tau, t_rise, t_drop, c_start, c_end, c_drop, c_rise), start = list(
+  tau = tau, t_rise = t_rise, t_drop = t_drop, c_start = c_start, c_end = c_end, c_drop = c_drop, c_rise = c_rise
+))
 
 ##debugging
 ##options(error=recover)
 ##she'll show u a list of how she proceeded and where she got stuck
-##
 
-
-##function can take more than one input
-##yay = 1
-#whoo <- function(input){}
-##whoo can call upon anyone outside of her function + whatever u designate as input
-##outside of function girlies are called global variables (anyone can call them for anything)
-##input is local, u can only call em w/in the function
 ##for optimisation, parameters need to be made local
+
+
+
+
+test_curve <- function(time, site, tau, t_rise, t_drop, c_start, c_end, c_drop, c_rise){
+  #u want to optimise girlies u dont know v well
+  #to optimise is to find the most likely value
+  
+  #if ur dependent on other guys dont get optimised outside
+  drop_m <- ((c_rise - c_drop)/(t_rise - t_drop))
+  drop_w <- c_drop - drop_m*t_drop #c of y = mx + c
+  
+  
+  out <- rep(c_start, length(time))
+  out[time<= t_drop] = c_start
+  out[time>t_drop & time<t_rise] =  drop_m * time[time>t_drop & time<t_rise] + drop_w
+  out[time == t_rise] = c_rise
+  out[time>t_rise] = (c_end - c_rise)*(1 - (exp(-tau*(time[time>t_rise] - t_rise)))) + c_rise
+  return(out) #makes her global
+}
+
+t1 <- print(test_curve(time = 15:23, site = test_site, tau = 3, t_rise = 22, t_drop = 19, c_start = 88, c_end = 83, c_drop = 82, c_rise = 30 ))
+t2 <- print(test_curve(time = 15:23, site = test_site, tau = 8, t_rise = 20, t_drop = 19, c_start = 78, c_end = 83, c_drop = 82, c_rise = 15 ))
+t3 <- print(test_curve(time = 15:23, site = test_site, tau = 0.1, t_rise = 20, t_drop = 17, c_start = 78, c_end = 83, c_drop = 82, c_rise = 15 ))
+t3.5 <- print(test_curve(time = 15:23, site = test_site, tau = 0.1, t_rise = 18, t_drop = 17, c_start = 78, c_end = 83, c_drop = 82, c_rise = 15 ))
+
+france <- test_site$tcc
+
+new_fit <- nls( france ~ test_curve(time = 15:23, site = test_site, tau, t_rise, t_drop, c_start, c_end, c_drop, c_rise), start = list(
+  tau = tau, t_rise = t_rise, t_drop = t_drop, c_start = c_start, c_end = c_end, c_drop = c_drop, c_rise = c_rise
+))
+
+#something cocked up in the event decreasing line equation (fixed)
+#during optimisation t drop can happen after t rise and we need to Stop That
